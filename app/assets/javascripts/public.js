79a00582c16c00a7ea5bf178ee661c3003aa0f7b
jdfdesign <regis@joufdesign.com>
2012-02-11 09:30:00 -0430

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,14 +1,14 @@
 //= require gko_store_public_all
-
+//= require twitter/bootstrap/carousel.js 
 
 jQuery(document).ready(function ($) {
-
-	if($('.product-images:first').length > 0) {
-		Gallery.init();
-	}
-	$('#features').orbit({ 
+	$('#orbit').orbit({ 
 		bullets : true, 
 		animation : "horizontal-push",
 		animationSpeed: 800,
 		timer: true });
+	if($('.product-images:first').length > 0) {
+		Gallery.init();
+	}
+ $('.carousel').carousel()
 });
