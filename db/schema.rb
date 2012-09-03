44ff5199cbefe7e01b1fe995aeac0baa1f697ef6
jdfdesign <regis@joufdesign.com>
2012-09-03 12:23:12 -0400

    bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120827084853) do
+ActiveRecord::Schema.define(:version => 20120903161910) do
 
   create_table "accounts", :force => true do |t|
     t.string   "reference",  :limit => 40
@@ -476,8 +476,10 @@ ActiveRecord::Schema.define(:version => 20120827084853) do
     t.boolean  "default",      :default => false
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
+    t.integer  "position",     :default => 1
   end
 
+  add_index "languages", ["site_id", "position"], :name => "index_languages_on_site_id_and_position"
   add_index "languages", ["site_id"], :name => "index_languages_on_site_id"
 
   create_table "line_items", :force => true do |t|
